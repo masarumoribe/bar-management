5ec8a43214f464a85c005299e1e41c9b974136d8
Masaru <user@USERnoMacBook-Pro.local>
2024-06-17 20:43:24 +0900

    added server folder for the backend of the app, installed mysql, express and sequelize

---
+++ client/src/components/Content.js
@@ -0,0 +1,36 @@
+import './Content.css'
+
+function Content({inventoryData}) {
+    console.log(inventoryData)
+    return(
+        <div className='main-content'>
+            {inventoryData && inventoryData.length > 0 ? <h1>Inventory Items</h1>: <h1>No invetory items</h1>}
+            <table className='invTable'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Unit</th>
+                        <th>Cost</th>
+                        <th>Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {inventoryData.map(invItem => {
+                return (
+                    <tr key={invItem.id}>
+                        <td>{invItem.name}</td>
+                        <td>{invItem.category}</td>
+                        <td>{invItem.unit}</td>
+                        <td>{invItem.cost}</td>
+                        <td>{invItem.quantity}</td>
+                    </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Content;
